b06c575f0ccdf1d4deb2d78e0d1abd60af74f228
jotadwright <54960963+jotadwright@users.noreply.github.com>
2023-05-22 20:12:14 +0200

    Add files via upload

--- icon_phon.py
+++ icon_phon.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import panphon
-import urllib
+from urllib import request
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
@@ -23,13 +23,14 @@ def flatten_list(_2d_list):
             flat_list.append(element)
     return flat_list
 
-def vec_norm(vec_list, max_len):
 
-    empty_vec = [0] * 24
-    while len(vec_list) < max_len:
-        vec_list.append(empty_vec)
+def vec_norm(vec_list):
+    new_list = []
+    for v in vec_list:
+        v_n = [0 if item == -1 else item for item in v]
+        new_list.append(v_n)
+    return new_list
 
-    return vec_list
 
 def vec_mean(vec_list):
     df = pd.DataFrame(vec_list)
@@ -38,41 +39,48 @@ def vec_mean(vec_list):
         vec_mean = [0] * 24
     return vec_mean
 
-ft = panphon.FeatureTable()
 
+def rescale(xmin, xmax, data):
+    data_scaled = np.interp(data, (xmin, xmax), (-1, 1))
+    return data_scaled
+
+
+ft = panphon.FeatureTable()
 
 # Load iconicity dataset
 url = "https://raw.githubusercontent.com/jotadwright/NLP_EX3/main/iconicity_ratings.csv"
-df = pd.read_csv(urllib.request.urlopen(url))
+df = pd.read_csv(request.urlopen(url))
 
 # Scale iconicity values
-scaler = MinMaxScaler((-1,1))
-df['Iconicity'] = scaler.fit_transform(df[['Iconicity']])
+df['Iconicity'] = df['Iconicity'].apply(lambda x: rescale(-5, 5, x))
 df = df.sort_values(by='Iconicity', ascending=False, ignore_index=True)
 
-
+# Get and normalise phonetic vectors
 df['phon_vec'] = df['Word'].apply(lambda x: ft.word_to_vector_list(x, numeric=True))
-max_len = max(df['phon_vec'].apply(len))
+df['phon_vec'] = df['phon_vec'].apply(vec_norm)
 
+# Get mean of phonetic vectors for each word
 df['phon_vec_mean'] = df['phon_vec'].apply(vec_mean)
-# print("MAX LEN MEAN")
-# print(max(df['phon_vec_mean'].apply(len)))
-# print("MIN LEN MEAN")
-# print(min(df['phon_vec_mean'].apply(len)))
 
-df['phon_vec'] = df['phon_vec'].apply(lambda x: vec_norm(x, max_len))
-df['phon_vec'] = df['phon_vec'].apply(flatten_list)
+# List phonetic features and choose which to keep
+phon_features = ['1_syl', '2_son', '3_cons', '4_cont', '5_delrel', '6_lat', '7_nas', '8_strid', '9_voi', '10_sg', '11_cg', '12_ant',
+                 '13_cor', '14_distr', '15_lab', '16_hi', '17_lo', '18_back', '19_round', '20_velaric', '21_tense', '22_long', '23_hitone', '24_hireg']
+
+keep = ['1_syl', '2_son', '3_cons', '4_cont', '6_lat', '7_nas', '8_strid', '9_voi', '12_ant',
+                 '13_cor', '14_distr', '15_lab', '16_hi', '17_lo', '18_back', '19_round', '21_tense']
+
+spl = pd.DataFrame(df['phon_vec_mean'].to_list(), columns=phon_features)
+df = df.join(spl)
+df = df.drop(['phon_vec', 'phon_vec_mean'], axis=1)
+
+df.to_csv('icon_phon.csv', index=False)
 
 
-print(type(df['phon_vec_mean'][0]))
-
-X = df['phon_vec_mean'].to_list()
+X = df[keep]
 y = df['Iconicity']
 
 # Split data in 70/30 test/train sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=33)
-
-print(X_train[0])
 
 # Load and fit the linear regression model to the training set.
 lm = linear_model.LinearRegression()
@@ -97,7 +105,17 @@ meanAbErr = metrics.mean_absolute_error(y_test, y_pred)
 meanSqErr = metrics.mean_squared_error(y_test, y_pred)
 rootMeanSqErr = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
 
-print('Test set metrics')
+
+
+ft_mean = df.mean(numeric_only=True, axis=0)
+
+print(f'Coefficients')
+for i,j in zip(keep,lm.coef_):
+    print(i,j)
+
+print('\nMean feature values', ft_mean)
+
+print('\n', 'Test set metrics')
 print('R squared: {:.2f}'.format(lm.score(X, y) * 100))
 print('Mean Absolute Error:', meanAbErr)
 print('Mean Square Error:', meanSqErr)
